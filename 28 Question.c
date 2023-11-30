75f3c4195a3c58d0832edcdf5edda3e62d0888d6
BhaveshKumar002 <123447145+BhaveshKumar002@users.noreply.github.com>
2023-11-30 09:49:08 +0530

    Create 28 Question.c

---
+++ 28 Question.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+uint64_t modExp(uint64_t a, uint64_t b, uint64_t m) {
+    uint64_t result = 1;
+    a = a % m;
+
+    while (b > 0) {
+        if (b % 2 == 1) {
+            result = (result * a) % m;
+        }
+
+        b = b >> 1;
+        a = (a * a) % m;
+    }
+
+    return result;
+}
+
+uint64_t diffieHellman(uint64_t privateKey, uint64_t publicKey, uint64_t prime) {
+    return modExp(publicKey, privateKey, prime);
+}
+
+int main() {
+  
+    uint64_t prime = 23;    
+    uint64_t primitiveRoot = 5;  
+
+    uint64_t alicePrivateKey = 6;
+    uint64_t bobPrivateKey = 15;
+    uint64_t alicePublicKey = modExp(primitiveRoot, alicePrivateKey, prime);
+    uint64_t bobPublicKey = modExp(primitiveRoot, bobPrivateKey, prime);
+
+    uint64_t sharedSecretAlice = diffieHellman(alicePrivateKey, bobPublicKey, prime);
+    uint64_t sharedSecretBob = diffieHellman(bobPrivateKey, alicePublicKey, prime);
+
+    printf("Alice's private key: %lu\n", alicePrivateKey);
+    printf("Bob's private key: %lu\n", bobPrivateKey);
+    printf("Public prime (q): %lu\n", prime);
+    printf("Primitive root (a): %lu\n", primitiveRoot);
+    printf("Alice's public key (ya): %lu\n", alicePublicKey);
+    printf("Bob's public key (yb): %lu\n", bobPublicKey);
+    printf("Shared secret key (Alice): %lu\n", sharedSecretAlice);
+    printf("Shared secret key (Bob): %lu\n", sharedSecretBob);
+
+    return 0;
+}
